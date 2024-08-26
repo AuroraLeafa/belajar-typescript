ed07de53a310dfea4f74e9ab8b783999831ad587
Refansyach <revansahdr@gmail.com>
2024-08-26 23:28:59 +0700

    Learn: array in TS

--- tests/data-types.test.ts
+++ tests/data-types.test.ts
@@ -1,3 +1,9 @@
+/*
+Always use watch command to run tests: npx tsc --watch
+Cause Jest will compile to JavaScript, so the test most likely will succeed
+with watch command, we will get an error if there's a syntax error
+*/
+
 describe('data-types', ():void => {
     //Primitive Data Types
     it("Should must declare", ():void => {
@@ -9,11 +15,33 @@ describe('data-types', ():void => {
     })
 
     //Arrays
-    it("Should must declare", ():void => {
+    it("Should must declare an array", ():void => {
         const names:string[] = ["Revvan", "Reffz"]
         const ages:number[] = [20, 21]
         const isMarried:boolean[] = [true, false]
         expect(names).toEqual(["Revvan", "Reffz"]);
         console.info(names, ages, isMarried)
     })
+
+    //Readonly Arrays
+    it("Should support Readonly Array", ():void => {
+        const names:ReadonlyArray<string> = ["Revvan", "Reffz"]
+        const ages:ReadonlyArray<number> = [20, 21]
+        const isMarried:ReadonlyArray<boolean> = [true, false]
+        names[1] = "Reffan"
+        ages[1] = 22
+        isMarried[1] = true
+        expect(names).toEqual(["Revvan", "Reffan"]);
+        console.info(names, ages, isMarried)
+    })
+
+    //Tuple
+    it("Should support Tuple", ():void => {
+        const tuple:readonly [string, number, string] = ["Refan", 23, "Testing"]
+        tuple[1] = 33
+        expect(tuple).toEqual(["Refan", 33, "Testing"]);
+        console.info(tuple)
+    })
+
+
 })
